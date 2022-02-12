86c322d9ad34777f6618498ec0e32605b9179fae
ShimulChandraDas <msshimul99@gmail.com>
2022-02-12 23:46:14 +0600

    Get all product sub total cost, tax and grand total

--- app.js
+++ app.js
@@ -7,14 +7,33 @@ function updateProductCount(product, price, isIncresing) {
     } else if (productCount > 0) {
         productCount = parseInt(productCount) - 1;
     }
-    productInput.value = productCount
+    productInput.value = productCount;
 
     // Update Total
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productCount * price;
+
+    //calculate total
+    calculateTotal();
 };
+// calculate total price 
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+};
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
 
-
+    // update on the sub Total 
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+};
 // phone increse & Decrtese
 document.getElementById('phone-plus').addEventListener('click', function () {
     updateProductCount('phone', 1219, true);
@@ -22,7 +41,6 @@ document.getElementById('phone-plus').addEventListener('click', function () {
 document.getElementById('phone-minus').addEventListener('click', function () {
     updateProductCount('phone', 1219, false);
 });
-
 // case increse & Decrtese
 document.getElementById('case-plus').addEventListener('click', function () {
     updateProductCount('case', 59, true);
