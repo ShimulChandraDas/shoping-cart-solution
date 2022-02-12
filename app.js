3477f8531c63e705d12ba600e13da312fc7c1234
ShimulChandraDas <msshimul99@gmail.com>
2022-02-12 21:30:32 +0600

    shopping cart handle decrease and update price

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+function updateCaseCount(isIncresing) {
     const caseInput = document.getElementById('case-count');
-    const caseCount = caseInput.value;
-    caseInput.value = parseInt(caseCount) + 1;
-    console.log(caseCount);
-})
+    let caseCount = caseInput.value;
+    if (isIncresing == true) {
+        caseCount = parseInt(caseCount) + 1;
+    } else if (caseCount > 0) {
+        caseCount = parseInt(caseCount) - 1;
+    }
+    caseInput.value = caseCount
+
+    // Update Case Total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseCount * 59;
+};
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseCount(true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseCount(false)
+});
